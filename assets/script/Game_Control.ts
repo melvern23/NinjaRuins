f30b268b638a36bfe774c4dfea9e2699dc831841
6182001018 - Benedictus Melvern <118327384+melvern23@users.noreply.github.com>
2024-04-11 11:24:06 +0700

    Perubahan Folder Enemy

--- assets/script/Game_Control.ts
+++ assets/script/Game_Control.ts
@@ -8,12 +8,16 @@ export class Game_Control extends Component {
     @property({type: Node}) monster1 : Node;
     @property({type:CCInteger})movement_speed;
     @property({type:CCInteger})jump_height;
+
+    go_left : number = -1;
+    go_right: number = 1;
+
     boy_movement_direction = 0;
     boy_position = new Vec3(-540,-140,0);
     boy_animation : Animation;
     boy_rigid : RigidBody2D;
     boy_collider : Collider2D;
-
+    boy_jumped : boolean;
 
     girl_movement_direction = 0;
     girl_position = new Vec3(-470,-140,0);
@@ -21,7 +25,8 @@ export class Game_Control extends Component {
     girl_rigid : RigidBody2D;
     girl_collider : Collider2D;
     
-    enemy_speed :number = -100;
+    enemy_speed :number = -500;
+    monster1_direction = 0;
     monster1_position = new Vec3(280,-140,0);
     monster1_collide:Collider2D;
     monster1_rigid:RigidBody2D;
@@ -36,6 +41,7 @@ export class Game_Control extends Component {
         this.boy_animation = this.boy_player.getComponent(Animation);
         this.boy_rigid = this.boy_player.getComponent(RigidBody2D);
         this.boy_collider = this.boy_player.getComponent(Collider2D);
+        this.boy_jumped = false;
 
         this.girl_animation = this.girl_player.getComponent(Animation);
         this.girl_rigid = this.girl_player.getComponent(RigidBody2D);
@@ -44,12 +50,13 @@ export class Game_Control extends Component {
         this.monster1.setPosition(this.monster1_position);
         this.monster1_collide = this.monster1.getComponent(Collider2D);
         this.monster1_rigid = this.monster1.getComponent(RigidBody2D);
+        this.monster1_direction
     }
     onKeyDown(event: EventKeyboard){
         switch(event.keyCode){
             //boy movement
             case KeyCode.KEY_A:
-                this.boy_movement_direction = -1;
+                this.boy_movement_direction = this.go_left;
                 this.boy_animation.play('boy_back');
                 break;
             case KeyCode.KEY_D:
@@ -58,23 +65,24 @@ export class Game_Control extends Component {
                 //     this.boy_animation.stop;
                 //     return;
                 // }
-                this.boy_movement_direction = 1;
+                this.boy_movement_direction = this.go_right;
                 this.boy_animation.play('boy_run');
                 break;
             case KeyCode.KEY_W:
-                if(this.boy_position.y<=-140){
+                if(!this.boy_jumped){
                     this.boy_animation.play('boy_jump');
                     this.boy_rigid.applyForceToCenter(v2(0,this.jump_height),true); // membuat player bisa lompat dengan parameter y = this.jump_height
+                    this.boy_jumped = true;
                 }
                 break;
 
             // girl movement
             case KeyCode.ARROW_LEFT:
-                this.girl_movement_direction = -1;
+                this.girl_movement_direction = this.go_left;
                 this.girl_animation.play('girl_back');
             break;
             case KeyCode.ARROW_RIGHT:
-                this.girl_movement_direction = 1;
+                this.girl_movement_direction = this.go_right;
                 this.girl_animation.play('girl_run');
                 break;
             case KeyCode.ARROW_UP:
@@ -93,8 +101,9 @@ export class Game_Control extends Component {
                 this.boy_movement_direction = 0;
                 this.boy_animation.stop();
                 break;
-            // case KeyCode.KEY_W:
-            //     this.boy_animation.stop();
+            case KeyCode.KEY_W:
+                this.boy_animation.stop();
+                this.boy_jumped = false;
             //girl movement
             case KeyCode.ARROW_LEFT:
             case KeyCode.ARROW_RIGHT: 
@@ -128,9 +137,16 @@ export class Game_Control extends Component {
         this.girl_rigid.linearVelocity = v2(this.girl_movement_direction * this.movement_speed * deltaTime, this.girl_rigid.linearVelocity.y);
         this.girl_position.x += this.girl_movement_direction * this.movement_speed * deltaTime;
 
-        this.monster1_rigid.linearVelocity = v2(this.enemy_speed * deltaTime, this.monster1_rigid.linearVelocity.y);
-        this.monster1_position.x -= this.enemy_speed * deltaTime;
-        
+        this.monster1_rigid.linearVelocity = v2(this.monster1_direction * this.enemy_speed * deltaTime, this.monster1_rigid.linearVelocity.y);
+        this.monster1_position.x += this.enemy_speed * deltaTime;
+
+        if(this.monster1_position.x <= -640){
+            this.monster1_direction = this.go_right;
+        }
+        if(this.monster1_position.x >= 3840){
+            this.monster1_direction = this.go_right;
+        }
+        // -640 buat monster berjalan kembali
 
     }
 }
